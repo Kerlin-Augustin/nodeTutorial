1f13a5372ff8232b9cc7b861b89309d3c0aedf08
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-06-28 11:55:11 -0400

    responses

--- server/server.js
+++ server/server.js
@@ -3,7 +3,15 @@ const http = require('http');
 // the createServer method creates a http server object
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel stylesheet="" href="#"></head>');
+    res.write('<p>hello world</p>');
+    res.write('<p>hello world, again</p>');
+    res.end();
 });
 
 // We are listening for requests here. 3000 is the port number, localhost is where it will listen to you request (note that the localhost arguement is optional if you don't put it it will be localhost by default, and the last arguemnet is a callback function to fire the server up.) Localhost is pointing to a ip address that is pointing to our own computer.
