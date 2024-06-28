5c8de08f482034c0fef839027022a86c4def3d06
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-06-28 15:59:07 -0400

    more express

--- express/app.js
+++ express/app.js
@@ -9,5 +9,22 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    // res.send('<h1>Home</h1>');
+    res.sendFile('/Users/kerlinaugustin/Desktop/nodeTutorial/server/views/index.html')
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>About</h1>');
+    res.sendFile('/Users/kerlinaugustin/Desktop/nodeTutorial/server/views/about.html')
+})
+
+// redirects
+app.get('/about-me', (req, res) => {
+    res.redirect('/about');
+})
+
+// 404 page
+
+app.use((req, res) => {
+    res.status(404).sendFile('/Users/kerlinaugustin/Desktop/nodeTutorial/server/views/404.html')
 })
