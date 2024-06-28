d38b818b9d230d5857a49e671db53f05754f0313
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-06-28 12:04:38 -0400

    responding with html

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 
+// fs is file system. This is a node module that allows us to read and write files.
+const fs = require('fs')
+
 // the createServer method creates a http server object
 
 const server = http.createServer((req, res) => {
@@ -8,10 +11,19 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel stylesheet="" href="#"></head>');
-    res.write('<p>hello world</p>');
-    res.write('<p>hello world, again</p>');
-    res.end();
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else {
+            // since we are not sending multiple files we could just condense this code down and pass in the data object directly to the res.end() method and it will work the same as passing in the data object to ther res.write method.
+
+            // res.write(data);
+            res.end(data);
+        }
+    })
+   
 });
 
 // We are listening for requests here. 3000 is the port number, localhost is where it will listen to you request (note that the localhost arguement is optional if you don't put it it will be localhost by default, and the last arguemnet is a callback function to fire the server up.) Localhost is pointing to a ip address that is pointing to our own computer.
